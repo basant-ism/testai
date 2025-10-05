fe363a88cdf96aea38fc5cd57e82d911277fa4b8
basant6.kumar <basant6.kumar@ril.com>
2025-10-05 19:10:30 +0530

    first

---
+++ src/test/java/com/basant/testai/TestaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basant.testai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
