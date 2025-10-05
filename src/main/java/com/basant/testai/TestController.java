e22b56e96e9eba7360c5cd132a175d93b47d4aa6
basant6.kumar <basant6.kumar@ril.com>
2025-10-05 19:35:00 +0530

    third

--- src/main/java/com/basant/testai/TestController.java
+++ src/main/java/com/basant/testai/TestController.java
@@ -3,11 +3,20 @@ package com.basant.testai;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class TestController {
 
     @GetMapping("/test")
     String test() {
-        return "Test";
+        Random random = new Random();
+        int y = random.nextInt(100);
+        Integer x = null;
+        if(y > 50) {
+            x = y;
+        }
+        int ans = 1000/x;
+        return "Test" + ans;
     }
 }
