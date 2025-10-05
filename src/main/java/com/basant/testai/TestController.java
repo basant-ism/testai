a196e12455381e6d76a9f9b1e9edb75ebbfff7ed
basant6.kumar <basant6.kumar@ril.com>
2025-10-05 19:27:12 +0530

    second

---
+++ src/main/java/com/basant/testai/TestController.java
@@ -0,0 +1,13 @@
+package com.basant.testai;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    String test() {
+        return "Test";
+    }
+}
